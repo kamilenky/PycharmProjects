ce8a7dac2c96b688fc0cf375e6091edfb4bdbe54
Kamila <kamilenkykamilenky@gmail.com>
2025-03-24 09:59:27 +0100

    excercise 5 - new

---
+++ Lekcia_5_cvicenie_KKassayova.py
@@ -0,0 +1,11 @@
+numbers = [1, 2, 4, -6, 7, 8, 100, -125, 11, 123]
+names = ["Petr", "Ales", "Honza", "Lenka", "Andrea", "Alice"]
+random_codes = ["1-okdsaaa", "0-nFnldd", "0-AA", "0-uwqqq", "2-ZSTh", "0-RKOcsxxx", "1-LwWtss", "0-cdKiddd", "2-KpAAaa", "3-sOdSxhcds"]
+
+# 1. pomocí cyklu for vypište čísla ze seznamu numbers, ale přeskočte záporná čísla.
+
+# 2. pomocí cyklu while vypište všechna jména, pokud narazíš na jméno 'Alice' cyklus ukonči
+
+# 3. pomocí list comprehension vytvoř nový list, který bude obsahovat pouze prvky z 'random_codes', které obsahují 0
+
+# Dobrovolny ukol pro pokrocile (nebude bodovan): Vypis všechny pod-seznamy s alespoň třemi po sobě jdoucími stejnými znaky v seznamu 'random_codes'
