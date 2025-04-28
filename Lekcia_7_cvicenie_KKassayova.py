142c347409f8747c9abcf3994d2bf927c61955dd
Kamila <kamilenkykamilenky@gmail.com>
2025-04-28 13:02:09 +0200

    excercise 7: input choose_username + notes

--- Lekcia_7_cvicenie_KKassayova.py
+++ Lekcia_7_cvicenie_KKassayova.py
@@ -27,3 +27,124 @@
 # Pomocí metody create_user vytvoř alespoň 4 různé uživatele. Hodnoty si zvol podle sebe přímo v programu.
 #
 # Nakonec vytvořené uživatele vytiskni pomocí cyklu a metody print_user_info.
+
+
+# input
+def choose_username() -> str:
+    for _ in range(1):
+        while True:
+            user_name = input("Please, choose your user name (at lesat 4 characters): ")
+            if len(user_name) >= 4:
+                # yield user_name
+                # break
+            else:
+                print("User name must be at least 4 characters long.")
+
+usernames = list(choose_username())
+
+print("usernames:")
+print(usernames)
+
+# input
+
+def is_adult (valid_age, minimum_age=17) -> bool:
+    valid_age = int(input("Please, add your age: "))
+    return valid_age > minimum_age
+
+upravit cez loop? vratit 4 x user name?
+
+
+# def create_user(0)
+
+def create_user(
+
+        return is_adult, is_name_valid
+        if statements
+        is_name_valid==True + is_adult==True > slovnik
+
+        else exceptions
+)
+
+#exceptions
+try:
+    def is_name_valid
+        if min_lenght== False
+except ValueError:
+    print("Minimum user name lenghth is 4 characters")
+
+try:
+    def is_adult
+        if minimum_age== False
+except ValueError:
+    print("Minimum age must be 18 years old.")
+
+
+def print_user_info(print_user, str):
+    print("Hello  ", print_user)
+
+def print_email_info(print_email, str):
+    print("Your email account ", print_email, "has been created.")
+
+
+
+def get_numbers () -> tuple [int, str]:
+    return int, "str"
+
+def work() ->
+    x,y = def_int ()
+    def_str("abc")
+    print(def3())
+
+work()
+
+
+yield
+
+
+
+#
+# is_adult(valid_age)
+
+
+
+
+
+# input username
+
+# def greet_user(name: str) -> None:
+#     print("Hello ", name)
+#
+# greet_user("Jano")
+#
+
+
+#
+# # input email
+# # input vek
+#
+# def age (input_age): -> int
+#     print (f"What is your age?: ".lower() (input_age)
+#
+# age ("What is your age?: ".lower(), age_input)
+# age_input  = input ("What is your age?: ").lower()
+# print(age_input)
+# def is_adult  -> int:
+#     x = age_input
+#
+#     return x >= 18
+# print
+
+# age validation
+# usename validation
+
+# create user 4x
+# check dictionary
+
+# print: users <> eroor message
+#
+#
+# from my_package.new import add
+# from my_package import new
+#
+# res = new.add(6, 6)
+# print(res)
