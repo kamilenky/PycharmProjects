dafe6f93652417ad54611dd5f6ae49aa8604a5d3
Kamila <kamilenkykamilenky@gmail.com>
2025-05-10 20:56:14 +0200

    Notes - Lecture 11

--- notes.py
+++ notes.py
@@ -22,3 +22,43 @@ f._Foo__something()
 # from Demos.security.lsastore import retrieveddata
 # from win32comext.mapi.emsabtags import PR_EMS_AB_LOCAL_INITIAL_TURN
 
+# L11
+class JobClassification:
+    def __init__(self, job_classification: str, working_hours: int) -> None:
+        self.job_classificaiton = job_classification
+        self.working_hours = working_hours
+
+    def AssignJobClassification(self) -> None:
+        print(f"Pracovné zaradenienie: {self.job.classification}")
+
+    def AssignWorkingHours(self) -> None:
+        print(f"Pracovný uväzok: {self.job.classification}")
+
+
+job_title_1 = JobClassification(job_classification="Sociálny pracovník", working_hours=40)
+job_title_2 = JobClassification(job_classification="Opatrovateľ", working_hours=20)
+print(f"Pracovné zaradenie: {job_title_1.job_classificaiton}")
+print(f"Pracovné zaradenie: {job_title_2.job_classificaiton}")
+job_title_1.AssignJobClassification()
+job_title_2.AssignJobClassification()
+
+
+class JobClassification:
+    def __init__(self, job_classification: str, working_hours: int) -> None:
+        self.job_classificaiton = job_classification
+        self.working_hours = working_hours
+
+    def AssignJobClassification(self) -> None:
+        print(f"Pracovné zaradenienie: {self.job_classificaiton}")
+
+    def AssignWorkingHours(self) -> None:
+        print(f"Pracovný uväzok: {self.working_hours}")
+
+
+job_title_1 = JobClassification(job_classification="Sociálny pracovník", working_hours=40)
+job_title_2 = JobClassification(job_classification="Opatrovateľ", working_hours=20)
+
+job_title_1.AssignJobClassification()
+job_title_1.AssignWorkingHours()
+job_title_2.AssignJobClassification()
+job_title_2.AssignWorkingHours()
