996253110dee510e8a3586a4ff27cdfd3c5c6353
Kamila <kamilenkykamilenky@gmail.com>
2025-05-10 20:54:22 +0200

    Notes - Lecture 17

---
+++ notes.py
@@ -0,0 +1,24 @@
+#formatovanie CTRL+ALT+L
+
+# L17
+numebers = [1,2,3]
+odd_nr = []
+
+for item in numbers:
+    if item %2 == 1:
+        odd_nr.append(item)
+
+numebers = [1,2,3]
+odd_nr = [for item in numbers if item %2 ==1]
+
+class Foo:
+    def __somtning(self):
+        print("a")
+
+f = Foo()
+f._Foo__something()
+
+
+# from Demos.security.lsastore import retrieveddata
+# from win32comext.mapi.emsabtags import PR_EMS_AB_LOCAL_INITIAL_TURN
+
